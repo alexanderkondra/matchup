4564f367e135e4f4bf772602d029e43aef99e36a
Alexander Kondra and Jess Doss <jess@>
2019-06-06 16:34:40 -0700

    add js functions, add bootstrap styling

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,23 @@ $(document).ready(function() {
   var favMovie = parseInt($("#favMovie").val());
   var favPasttime = parseInt($("#favPasttime").val());
   var favColor = parseInt($("#favColor").val());
+  var gender = parseInt($("#gender").val());
 
-  var total = goodDate + favMovie + favPasttime + favColor;
+  var total = goodDate + favMovie + favPasttime + favColor + gender;
+
   if (total <=4) {
       $(".baggins").show();
-    } else if (total <=8)  {
+    }
+    else if (total <=16)  {
       $(".ariel").show();
-    } else if (total <=12) {
+
+    } else if (total <=44) {
       $(".darko").show();
+
     } else {
       $(".nomatch").show();
     }
+
     event.preventDefault();
   });
 });
